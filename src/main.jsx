10398d5c92ca26642bd8acef0b9a2299d2245e82
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-04-15 12:12:58 +0100

    Add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingPage from './pages/Landingpage/Landingpage';
+import ResultsPage from "./pages/Resultspage/Resultspage";
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />
+      },
+
+      {
+        path: "/results",
+        element: <ResultsPage />
+      }
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
